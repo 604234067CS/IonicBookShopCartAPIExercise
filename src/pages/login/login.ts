21e7e05b966edccc14bf6b69e1468bd2beb72080
604234067CS <604234067@paritchat.skru.ac.th>
2019-03-15 14:40:38 +0700

    add pagelogin

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -3,12 +3,6 @@ import { NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
 import { RegisterPage } from '../register/register';
 
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @Component({
   selector: 'page-login',
